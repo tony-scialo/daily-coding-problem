a3933cf64899ba211d802f2165c736a3f823929c
Scialo, Tony <tony_scialo@csx.com>
2018-11-06 19:23:40 -0500

    challenge 1 brute force attempt working

--- daily-coding-problem/src/app/app.module.ts
+++ daily-coding-problem/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChallengeOneComponent } from './challenge-one/challenge-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChallengeOneComponent
   ],
   imports: [
     BrowserModule
---
+++ daily-coding-problem/src/app/challenge-one/challenge-one.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-one',
+  templateUrl: './challenge-one.component.html',
+  styleUrls: ['./challenge-one.component.css']
+})
+export class ChallengeOneComponent implements OnInit {
+  k = 17;
+  test1 = [10, 15, 3, 7];
+  isTest1Valid: boolean;
+
+  test2 = [11, 15, 3, 7];
+  isTest2Valid: boolean;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.isTest1Valid = this.isAnyTwoNumbersAddToKBruteForce(
+      this.test1,
+      this.k
+    );
+
+    this.isTest2Valid = this.isAnyTwoNumbersAddToKBruteForce(
+      this.test2,
+      this.k
+    );
+  }
+
+  isAnyTwoNumbersAddToKBruteForce(numbers: number[], k: number): boolean {
+    for (let x = 0; x < numbers.length; x++) {
+      for (let y = 1; y < numbers.length; y++) {
+        if (numbers[x] + numbers[y] === k) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
