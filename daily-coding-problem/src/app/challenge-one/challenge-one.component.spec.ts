600da58c6e202f0d6f19d28f399478b607a00048
Scialo, Tony <tony_scialo@csx.com>
2018-11-09 20:44:11 -0500

    update app to ng 7 and added first test

--- daily-coding-problem/src/app/challenge-one/challenge-one.component.spec.ts
+++ daily-coding-problem/src/app/challenge-one/challenge-one.component.spec.ts
@@ -8,9 +8,8 @@ describe('ChallengeOneComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChallengeOneComponent ]
-    })
-    .compileComponents();
+      declarations: [ChallengeOneComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +20,11 @@ describe('ChallengeOneComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to handle the test m = [10, 15, 3, 7] and k = 17 to return true', () => {
+    const m = [10, 15, 3, 7];
+    const k = 17;
+    expect(component.isAnyTwoNumbersAddToKBruteForce(m, k)).toBeTruthy();
   });
 });
